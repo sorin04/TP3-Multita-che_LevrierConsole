f8ce2987988ae171e6d6ce13d60a3eb2163a5f6b
spirgari <sorin.pirgari@lyceeastier.com>
2024-11-27 17:13:36 +0100

    Add project files.

---
+++ TP3 – Multitâche_Levrier/Levrier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace CourseDeLevrier
+{
+    class Levrier
+    {
+        private static Random random = new Random();
+        private int numero;
+        private AutoResetEvent eventArrivee;
+
+        public Levrier(int numero, AutoResetEvent eventArrivee)
+        {
+            this.numero = numero;
+            this.eventArrivee = eventArrivee;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Lévrier {numero} commence la course.");
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(random.Next(1, 10));
+                Console.WriteLine($"Lévrier {numero} a parcouru {i} mètres.");
+
+                if (i % 1000 == 0)
+                {
+                    
+                    Console.WriteLine($"Lévrier {numero} est arrivé !");
+                }
+            }
+
+            
+            eventArrivee.Set();
+        }
+    }
+}
---
+++ TP3 – Multitâche_Levrier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace CourseDeLevrier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrez le nombre de Lévriers dans la course : ");
+            int nombreLeveriers = int.Parse(Console.ReadLine());
+            List<AutoResetEvent> events = new List<AutoResetEvent>();
+            List<Thread> threads = new List<Thread>();
+            for (int i = 1; i <= nombreLeveriers; i++)
+            {
+                AutoResetEvent eventArrivee = new AutoResetEvent(false);
+                events.Add(eventArrivee);
+                Levrier leverier = new Levrier(i, eventArrivee);
+                Thread thread = new Thread(leverier.Run);
+                threads.Add(thread);
+                thread.Start();
+            }
+            foreach (var eventArrivee in events)
+            {
+                eventArrivee.WaitOne();
+            }
+
+            Console.WriteLine("La course est terminée !");
+        }
+    }
+}
